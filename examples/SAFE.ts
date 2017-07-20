faf8913a808aad55573df1373a68c69ab4a8b991
meng wong <mengwong@pobox.com>
2017-07-20 21:01:59 +0100

    not quite working -- almost there

--- examples/SAFE.ts
+++ examples/SAFE.ts
@@ -1,18 +1,11 @@
-
-// 4 Change of Control Voting Block Threshold
-// 4 Company Legal Form
-// 4 Company Name
-// 2 Discount Rate
-// 1 Equity Financing Purchase Price Threshold
-// 4 Governing Law Jurisdiction
-// 4 Investor Name
-// 1 Liquidity Price Numerator
-// 4 Purchase Amount
-// 4 Purchase Date
-// 4 Time Notice is Effective after Deposit in the Mail
-// 2 Valuation Cap
+#!/usr/bin/env node
 
 // http://choly.ca/post/typescript-json/ may be relevant one day
+
+let cftemplate = require('../')
+var fs = require('fs')
+var glob = require('glob')
+var path = require('path')
 
 interface Common {
     "Change of Control Voting Block Threshold": string, // percentage string, ends with %
@@ -34,19 +27,40 @@ interface controlJSON { cap: boolean, discount: boolean }
 
 enum Species { Cap, Discount, CapDiscount, MFN }
 
-function nonzero(input : string): boolean {
+function nonzero(input: string): boolean {
     return (Number(input) > 0);
 }
 
-export function controller(userJSON) {
-    let guessedSpecies = guessSpecies(userJSON);
-    let validate_rv = validate(guessedSpecies, userJSON); // should be either a Cap, Discount, CapDiscount, or MFN -- can typescript do that?
+function read(file) {
+    return fs.readFileSync(file).toString()
 }
 
-function guessSpecies(userJSON: Common) : Species {
+export function controller(userJSON) {
+    let [controlJSON, guessedSpecies] = guessSpecies(userJSON);
+    let [validationError, validation_rv] = validate(guessedSpecies, controlJSON, userJSON); // should be either a Cap, Discount, CapDiscount, or MFN -- can typescript do that?
+    if (validationError) {
+        console.log("VALIDATION ERROR: " + validation_rv);
+    }
+    else {
+        // turn the cftemplate into a commonform
+
+        var mergedJSON = {};
+        Object.assign(mergedJSON, userJSON, controlJSON);
+
+        console.log("running cftemplate now! wish me luck!");
+
+        cftemplate(read('SAFE.cftemplate'),
+            ".",
+            mergedJSON);
+
+        // render the commonform into output
+    }
+}
+
+function guessSpecies(userJSON: Common): [controlJSON, Species] {
     let controlJSON: controlJSON = {
-        cap  : false,
-        discount : false
+        cap: false,
+        discount: false
     };
     let species: Species;
 
@@ -62,13 +76,19 @@ function guessSpecies(userJSON: Common) : Species {
     else if (controlJSON.cap) { species = Species.Cap }
     else { species = Species.MFN }
 
-    return species
+    return [controlJSON, species]
 }
 
-function validate(species, json) {
+function validate(species, controljson, userjson): [boolean, string] {
     // does typescript approve of the JSON object?
     // or should we be using http://json-schema.org/
+    return [false, "this is fine."];
 }
 
+import stdio = require("stdio");
+let opt = stdio.getopt();
+console.log("moo");
 
+let userjson = read("SAFE.user.json");
 
+controller(userjson);
